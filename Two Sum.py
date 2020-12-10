9dace2d005a7e4f4a3e826f77d97612ec8579588
harshitpoddar09 <53508456+harshitpoddar09@users.noreply.github.com>
2020-12-10 22:53:51 +0530

    Add files via upload

--- Two Sum.py
+++ Two Sum.py
@@ -1,6 +1,4 @@
-Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Solution:
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ans=[]
         for i in range(len(nums)):
