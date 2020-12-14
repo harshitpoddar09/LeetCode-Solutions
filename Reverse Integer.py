779257a9f01e29e881f492698628350b8eb6ec7a
harshitpoddar09 <53508456+harshitpoddar09@users.noreply.github.com>
2020-12-14 11:42:18 +0530

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,3 +1,4 @@
+# Method 1
 class Solution:
     def reverse(self, x: int) -> int:
         minimum=-(2**31)
@@ -15,3 +16,16 @@ class Solution:
             return 0
         else:
             return int(reverse_num)
+
+# Method 2
+class Solution:
+    def reverse(self, x: int) -> int:
+        minimum=-(2**31)
+        maximum=(2**31)-1
+        if x<0:
+            reverse_num=int(str(x)[:0:-1])*(-1)
+        else:
+            reverse_num=int(str(x)[::-1])
+        if reverse_num>maximum or reverse_num<minimum:
+            return 0
+        return reverse_num
