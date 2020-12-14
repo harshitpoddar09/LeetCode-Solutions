c61a2ec75b766c36c2d9926eb001d93aa9a00ca5
harshitpoddar09 <53508456+harshitpoddar09@users.noreply.github.com>
2020-12-14 11:38:54 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,3 +1,5 @@
+# This takes 1088ms to run
+"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ans=[]
@@ -9,4 +11,18 @@ class Solution:
                         ans.append(j)
         ans.pop(3)
         ans.pop(2)
+        return ans
+"""
+
+#This takes 40ms to run
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans=[]
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j:
+                    if nums[i]+nums[j]==target:
+                        ans.append(i)
+                        ans.append(j)
+                        return ans
         return ans
