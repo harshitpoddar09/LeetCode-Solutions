cf0c8af5457d4fdac0aa198dc05079251ceb45ae
harshitpoddar09 <53508456+harshitpoddar09@users.noreply.github.com>
2020-12-15 11:30:37 +0530

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        #Method 1 - 76 ms
         original_num=[]
         for i in str(x):
             original_num.append(i)
@@ -11,3 +12,21 @@ class Solution:
             print('false')
         else:
             return 'true'
+        
+        #Method 2 - 44ms 
+        return str(x)==str(x)[::-1]
+        
+        #Method 3 - without converting int to str
+        ori_x=x
+        if x>=0:
+            rev_x=0
+            for i in range(len(str(x))-1,-1,-1):
+                a=x%10
+                x=x//10
+                rev_x+=a*(10**i)
+            if ori_x==rev_x:
+                return 'true'
+            else:
+                print('false')
+        else:
+            print('false')
