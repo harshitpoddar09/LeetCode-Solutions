f441e4c4e19e5963dd4f734807c006d22f53a48d
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-12 10:21:05 +0530

    Update Search Insert Position.py

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,3 +1,5 @@
+#Method 1
+"""
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         for i in range(len(nums)):
@@ -8,3 +10,14 @@ class Solution:
                 ans=i
                 break 
         return ans
+"""
+
+#Method 2, faster and takes less space
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums[len(nums)-1]<target:
+            return len(nums)
+        else:
+            for i in range(len(nums)):
+                if nums[i]>=target:
+                    return i 
