612bc848350e4788a6296c9efd725bb2c1d97e95
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-13 21:22:19 +0530

    Update Search a 2D Matrix.py

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -1,3 +1,4 @@
+#Method1
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         num=[]
@@ -9,3 +10,15 @@ class Solution:
                 return 'true'
         else:
             print('false')
+            
+#Method2
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        num=[]
+        ans=0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j]==target:
+                    ans=1
+                    break
+        return ans
