f95cbbbcd146651084e45d23dd324077f934d502
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-19 09:51:10 +0530

    Update Find Minimum in Rotated Sorted Array II.py

--- Find Minimum in Rotated Sorted Array II.py
+++ Find Minimum in Rotated Sorted Array II.py
@@ -1,3 +1,10 @@
+#Submission 1 (faster)
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         return min(nums)
+    
+#Submission 2
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[0]
