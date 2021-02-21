53bc2d1dcd79f98f67058ae0cbeaea0abe6e042a
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-21 20:50:53 +0530

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,3 +1,9 @@
+#Submission 2
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return sorted(set(nums))!=sorted(nums)
+
+#Submission 1
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         a=set(nums)
