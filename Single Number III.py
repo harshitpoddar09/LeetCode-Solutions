200ff451a2b4d54a892c2c8818bda4e5c9616584
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-24 20:10:05 +0530

    Add files via upload

---
+++ Single Number III.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in set(nums):
+            if nums.count(i)==1:
+                ans.append(i)
+        return ans
