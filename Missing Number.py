34cabc2c7c1e2f785164c223209c89f5317a0345
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-02-26 10:04:00 +0530

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -1,3 +1,14 @@
+#Submission 2 - 132 ms
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        if len(nums) not in nums:
+            return len(nums)
+        for i in range(len(nums)+1):
+            if i!=nums[i]:
+                return i
+
+#Submission 1 - 2420ms
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         for i in range(len(nums)+1):
