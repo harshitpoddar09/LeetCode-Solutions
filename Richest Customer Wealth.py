7714c201b9d2ddaef729cf0486a9f0ad3c7fc198
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-15 09:26:46 +0530

    Update Richest Customer Wealth.py

--- Richest Customer Wealth.py
+++ Richest Customer Wealth.py
@@ -1,3 +1,15 @@
+"""
+#Submission 2
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        ans=0
+        for i in accounts:
+            if sum(i)>ans:
+                ans=sum(i)
+        return ans
+"""
+
+#Submission 1
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         total=[]
