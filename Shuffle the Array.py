2fc53aaf74b1d1f9d4e7619277a8e8fe0848022b
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-03-26 09:48:39 +0530

    Update Shuffle the Array.py

--- Shuffle the Array.py
+++ Shuffle the Array.py
@@ -1,3 +1,15 @@
+#Submission 2 (14.4mb)
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans=nums[:n]
+        j=1
+        for i in nums[n:]:
+            ans.insert(j,i)
+            j+=2
+        return ans
+    
+    
+#Submission 1 (14.5mb)
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         ans=[]
