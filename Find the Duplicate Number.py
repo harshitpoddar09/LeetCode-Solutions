d8c834dd724acf2b4ac80e34a9057cafadb69919
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-04-14 00:35:55 +0530

    Update Find the Duplicate Number.py

--- Find the Duplicate Number.py
+++ Find the Duplicate Number.py
@@ -1,3 +1,12 @@
+#Submission2
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return nums[i]
+ 
+#Submission1
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         for i in nums:
