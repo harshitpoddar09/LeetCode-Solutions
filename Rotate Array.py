328085cc921842e25f3135ef5f4d36fced706c81
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-04-17 09:18:14 +0530

    Add files via upload

---
+++ Rotate Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k%len(nums)):
+            nums.insert(0,nums[-1])
+            nums.pop()
