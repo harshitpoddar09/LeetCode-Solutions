7030f37f6490606f242fb8cc1e0103bc13789962
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-05-18 23:47:00 +0530

    Add files via upload

---
+++ Repeated Substring Pattern.py
@@ -0,0 +1,6 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        for i in range(1,(len(s)//2)+1):
+            if s[:i]*(len(s)//i)==s:
+                return 1
+        return 0
