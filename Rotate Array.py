612f067554e6ee1e0e103f975df45a6583770bde
Harshit Poddar <harshitpoddar09@gmail.com>
2021-07-02 21:54:17 +0530

    Update Rotate Array.py

--- Rotate Array.py
+++ Rotate Array.py
@@ -1,3 +1,13 @@
+#Submission 2 - 220ms 25.7mb
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
+
+#Submission 1 - 3108ms 25.9mb
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
