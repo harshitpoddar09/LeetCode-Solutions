d03240b5a56f5c048a4deac3f523f3328c130ab1
Harshit Poddar <harshitpoddar09@gmail.com>
2021-09-19 20:31:08 +0530

    Add files via upload

---
+++ LRU Cache.py
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.d=OrderedDict()
+        self.capacity=capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.d:
+            return -1
+        self.d.move_to_end(key)
+        return self.d[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self.d.move_to_end(key)
+        self.d[key]=value
+        if len(self.d)>self.capacity:
+            self.d.popitem(last=False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
