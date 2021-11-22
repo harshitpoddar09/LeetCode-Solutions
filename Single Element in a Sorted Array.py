d789a387279d064d7a6070ff4b8ab30c02a81e6f
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-22 11:14:01 +0530

    Update Single Element in a Sorted Array.py

--- Single Element in a Sorted Array.py
+++ Single Element in a Sorted Array.py
@@ -1,3 +1,27 @@
+#Submission 3 - 68ms
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l=0
+        r=len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if (mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2==1 and nums[mid]==nums[mid-1]):
+                l=mid+1
+            else:
+                r=mid
+        return nums[l]
+
+#Submission 2 - 60ms
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        i=0
+        while i+1<len(nums):
+            if nums[i]!=nums[i+1]:
+                return nums[i]
+            i+=2
+        return nums[-1]
+
+#Submission 1 - 4988ms
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         for i in nums:
