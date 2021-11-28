013b83a93c741c16488c02bce67d33ab85cb12fc
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-28 21:27:38 +0530

    Add files via upload

---
+++ Remove Colored Pieces if Both Neighbors are the Same Color.py
@@ -0,0 +1,11 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        alice=0
+        bob=0
+        for i in range(1,len(colors)-1):
+            if colors[i]==colors[i+1]==colors[i-1]:
+                if colors[i]=='A':
+                    alice+=1
+                else:
+                    bob+=1
+        return alice>bob
