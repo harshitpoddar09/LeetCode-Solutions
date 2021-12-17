9a195bcb806731d88fa446baade0d943b2f91545
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-17 21:23:58 +0530

    Add files via upload

---
+++ Minimum Cost to Move Chips to The Same Position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even=0
+        odd=0
+        for i in position:
+            if i%2==0:
+                even+=1
+            else:
+                odd+=1
+        return min(even,odd)
