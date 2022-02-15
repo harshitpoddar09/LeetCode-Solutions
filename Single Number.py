7867f72608bb23b7d245aaf8b2872ad4bb875644
Harshit Poddar <harshitpoddar09@gmail.com>
2022-02-15 22:22:33 +0530

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -1,3 +1,32 @@
+#Submission 4 - 247 ms 16.7 mb
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for i in nums:
+            xor = xor^i
+        return xor
+
+#Submission 3 - 168 ms 17.1 mb
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+        for i in s:
+            return i
+
+#Submission 2 - 162 ms 16.6 mb
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = Counter(nums)
+        for key in d:
+            if d[key] == 1:
+                return key
+
+#Submission 1 - 5700 ms 16.5 mb
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         for i in nums:
